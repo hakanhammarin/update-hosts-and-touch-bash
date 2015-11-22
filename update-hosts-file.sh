fa21074098b8f707227949ccbc64e622672f09db
hakanhammarin <hakan.hammarin@gmail.com>
2015-11-22 18:28:55 +0100

    begin with MySQL export from Cacti

--- update-hosts-file.sh
+++ update-hosts-file.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
+# mysql -B -D mydatabase -e 'select * from mytable' | sed -e 's/\t/,/g'
+# mysql --user=wibble --password wobble -B -e "select * from vehicle_categories;" | sed "s/'/\'/;s/\t/\",\"/g;s/^/\"/;s/$/\"/;s/\n//g" > vehicle_categories.csv
+
 INPUT=data.csv
 OLDIFS=$IFS
 IFS=,
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while read IP NAME 
+while read IP NAME
 do
 	echo "IP : $IP"
 	echo "NAME : $NAME"
-	ping -c 1 -t 1 "$IP" 
+	ping -c 1 -t 1 "$IP"
 	echo "Exit code = $?"
 	touch "$NAME"
 	if grep -e $NAME hosts.txt > /dev/null; then
    echo "Found"
-   sed -i .bak '/'"$NAME"'/ s/.*/'"$IP $NAME #Updated $(date)"'/g' hosts.txt 
+   sed -i .bak '/'"$NAME"'/ s/.*/'"$IP $NAME #Updated $(date)"'/g' hosts.txt
 else
 		echo "$IP $NAME # New $(date)" >> hosts.txt
 fi
-	
+
 	# echo "sed Exit code = $?"
 	# if not [ $? == 0 ]
-	# 	then 
-	
+	# 	then
+
 	# fi
 
 
