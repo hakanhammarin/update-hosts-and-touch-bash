7f710b9440fcf99f24cf53b9f195a1641ade8009
hakanhammarin <hakan.hammarin@gmail.com>
2015-11-23 16:16:43 +0100

    ok

--- update-hosts-file.sh
+++ update-hosts-file.sh
@@ -1,36 +1,67 @@
-#!/bin/bash
-# mysql -B -D mydatabase -e 'select * from mytable' | sed -e 's/\t/,/g'
-# mysql --user=wibble --password wobble -B -e "select * from vehicle_categories;" | sed "s/'/\'/;s/\t/\",\"/g;s/^/\"/;s/$/\"/;s/\n//g" > vehicle_categories.csv
+#
+DATAFILE=data.csv
+USERFILE=users.txt
+cd /var/update-hosts/
+touch $DATAFILE
+touch $USERFILE
+nmap -oG nmap.log -sP 10.0.0.0/24
 
-INPUT=data.csv
+
+cat nmap.log | sed "s/'/\'/;s/Host: //g;s/^//;s/$//;s/\n//g"  | sed "s/'/\'/;s/ .*//g;s/^//;s/$//;s/\n//g" | sed "s/'/\'/;s/#.*/1\.1\.1\.1/g;s/^//;s/$//;s/\n//g" > $DATAFILE
+
+
+## Working MySQL ##
+# mysql --user=cactimonitor -p********** cacti -B -e "SELECT hostname,description FROM cacti.host;" | sed "s/'/\'/;s/[,() ]/-/g;s/^//;s/$//;s/\n//g" | sed "s/'/\'/;s/\t/,/g;s/^//;s/$//;s/\n//g" | awk '{print tolower($0)}' > $DATAFILE
+
+#
+
+ls /home/ | grep - > $USERFILE
+#
+INPUT=$DATAFILE
 OLDIFS=$IFS
 IFS=,
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while read IP NAME
+#while read IP NAME #For Cacti
+
+while read IP
 do
 	echo "IP : $IP"
+#	echo "Cacti NAME : $NAME"
+	SYSNAME=$(/usr/bin/snmpwalk -OUEvqt -t 1 -r 0 -c ***************** -v2c $IP SNMPv2-MIB::sysName.0 | awk '{print tolower($0)}' | sed "s/'/\'/;s/\..*//g;s/^//;s/$//;s/\n//g" )
+	echo "snmpwalk sysName : $SYSNAME"
+	touch "/var/update-hosts/touch-files/$SYSNAME"
+
+	echo "$IP,$SYSNAME,$(date)" >> hosts.csv	
+
+
+#	if grep -e "$SYSNAME" hosts.txt > /dev/null; then
+	if grep -e "$SYSNAME" /etc/hosts > /dev/null; then
+
+		echo "Found in hosts"
+		sed -i '/'"$SYSNAME"'/ s/.*/'"$IP $SYSNAME #Updated $(date)"'/' /etc/hosts
+
+	else
+		echo "$IP $SYSNAME # New $(date)" >> /etc/hosts
+		echo "NOT Found in hosts"
+	fi
+
+
+done < $INPUT
+
+
+
+
+IFS=$OLDIFS
+
+
+INPUT=$USERFILE
+OLDIFS=$IFS
+IFS=,
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read NAME
+do
 	echo "NAME : $NAME"
-	ping -c 1 -t 1 "$IP"
-	echo "Exit code = $?"
-	touch "$NAME"
-	if grep -e $NAME hosts.txt > /dev/null; then
-   echo "Found"
-   sed -i .bak '/'"$NAME"'/ s/.*/'"$IP $NAME #Updated $(date)"'/g' hosts.txt
-else
-		echo "$IP $NAME # New $(date)" >> hosts.txt
-fi
-
-	# echo "sed Exit code = $?"
-	# if not [ $? == 0 ]
-	# 	then
-
-	# fi
-
-
-	#! /bin/bash
-
-# IP=$(dig +short myIP.opendns.com @resolver1.opendns.com)
-
-# HOST="peep.strudel.com"
+	cp /var/update-hosts/touch-files/* /home/$NAME
+	
 done < $INPUT
 IFS=$OLDIFS
